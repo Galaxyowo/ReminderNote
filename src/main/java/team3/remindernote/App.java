d5138319e7804d152e65ae50e79b03ed84fc7c7f
Galaxyowo <133757941+Galaxyowo@users.noreply.github.com>
2024-03-11 21:16:20 +0700

    Fix fxml

--- src/main/java/team3/remindernote/App.java
+++ src/main/java/team3/remindernote/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("FirstTime"));
+        scene = new Scene(loadFXML("AllNote"));
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
